12f0b1b8c52ea0ece43bc08adce7bb05a171aad4
mo zaki <mo.zaki@elecfron.com>
2025-07-28 00:22:30 +0100

    Add latest 27-07-25 sent by yousif

--- src/StarterGui/ButtonsGUI.lua
+++ src/StarterGui/ButtonsGUI.lua
@@ -1,6 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local sendSignal = ReplicatedStorage:WaitForChild("30SecondSignal")
-local buttonsGui = script.Parent  -- assuming this script is inside the Buttons GUI
+local buttonsGui = script.Parent  -- assuming this script is inside the Buttons GUI fff
 
 sendSignal.OnClientEvent:Connect(function()
 	buttonsGui.Enabled = true
