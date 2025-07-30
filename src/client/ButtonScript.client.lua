7193cad0e2f91eada320f1c2449dc9833bcc8c86
agbfrank <cutieyousie3@gmail.com>
2025-07-30 16:12:14 +0100

    staging the changes and pushing sice last commit did not push

---
+++ src/client/ButtonScript.client.lua
@@ -0,0 +1,23 @@
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+local buttonsGui = script.Parent
+local disableGUIEvent = ReplicatedStorage:WaitForChild("DisableButtonsGUI")
+
+-- Make sure ButtonsGUI is initially disabled
+buttonsGui.Enabled = false
+
+-- Listen for countdown start (30SecondSignal)
+local sendSignal = ReplicatedStorage:WaitForChild("30SecondSignal")
+sendSignal.OnClientEvent:Connect(function()
+	if player.Team == nil or player.Team.Name == "Neutral" then
+		buttonsGui.Enabled = true
+	end
+end)
+
+-- Disable GUI if team has been picked
+disableGUIEvent.OnClientEvent:Connect(function()
+	buttonsGui.Enabled = false
+end)
