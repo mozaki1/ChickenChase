c2521cf5745653cba73b445ea9f82ef4f44e1fdb
mo zaki <mo.zaki@elecfron.com>
2025-08-11 18:51:10 +0100

    Removing the textlable attribute

--- src/client/CountdownScriptGUI.client.lua
+++ src/client/CountdownScriptGUI.client.lua
@@ -5,13 +5,13 @@ local function UpdateStatus()
 	local gameStatus = workspace:GetAttribute("Status")
 
 	if gameStatus == "In-Game" or gameStatus == "Game starting in" then
-		gui.TextLabel.Text = (gameStatus == "In-Game" and "Game ending in: " or "Game starting in: ") .. clockTime
+		gui.Text = (gameStatus == "In-Game" and "Game ending in: " or "Game starting in: ") .. clockTime
 	else
-		gui.TextLabel.Text = gameStatus
+		gui.Text = gameStatus
 	end
 end
 
 workspace:GetAttributeChangedSignal("Clock"):Connect(UpdateStatus)
 workspace:GetAttributeChangedSignal("Status"):Connect(UpdateStatus)
 
-gui.TextLabel.Text = "Game starting..."
+--gui.Text = "Game starting..."
