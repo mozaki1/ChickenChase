ba0244ca2feaa23969872b944c0eef3986334f79
agbfrank <cutieyousie3@gmail.com>
2025-08-11 19:29:47 +0100

    fixed countdown scripts

--- src/client/CountdownScriptGUI.client.lua
+++ src/client/CountdownScriptGUI.client.lua
@@ -1,17 +1,28 @@
-local gui = script.Parent
+local label = script.Parent
+label.BackgroundTransparency = 1
+label.TextScaled = true
+label.Visible = false
+label.TextColor3 = Color3.new(0, 0, 0)
+label.TextStrokeTransparency = 0
 
-local function UpdateStatus()
-	local clockTime = workspace:GetAttribute("Clock")
-	local gameStatus = workspace:GetAttribute("Status")
+local event = game.ReplicatedStorage:WaitForChild("StartCountdown")
 
-	if gameStatus == "In-Game" or gameStatus == "Game starting in" then
-		gui.TextLabel.Text = (gameStatus == "In-Game" and "Game ending in: " or "Game starting in: ") .. clockTime
-	else
-		gui.TextLabel.Text = gameStatus
+event.OnClientEvent:Connect(function(msg)
+	label.Visible = true
+
+	if typeof(msg) == "number" then
+		if msg == 0 then
+			label.Text = ""
+		else
+			label.Text = "game starts in " .. msg .. " second" .. (msg == 1 and "" or "s")
+		end
+
+	elseif typeof(msg) == "string" then
+		label.Text = msg
+
+		if msg == "game starting!" then
+			task.wait(2)
+			label.Visible = false
+		end
 	end
-end
-
-workspace:GetAttributeChangedSignal("Clock"):Connect(UpdateStatus)
-workspace:GetAttributeChangedSignal("Status"):Connect(UpdateStatus)
-
-gui.TextLabel.Text = "Game starting..."
+end)
