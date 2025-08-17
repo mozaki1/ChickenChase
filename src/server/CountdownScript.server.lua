5a230be42cc9a3cc81a4b3ea65261fa4d3ea3738
mo zaki <mo.zaki@elecfron.com>
2025-08-17 22:34:08 +0100

    Added a catch script

--- src/server/CountdownScript.server.lua
+++ src/server/CountdownScript.server.lua
@@ -1,11 +1,11 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local StartCountdown = ReplicatedStorage:FindFirstChild("StartCountdown")
+local StartCountdown = ReplicatedStorage:FindFirstChild("StartCountdown") or Instance.new("RemoteEvent", ReplicatedStorage)
+StartCountdown.Name = "StartCountdown"
 
-
-local sendSignal = ReplicatedStorage:FindFirstChild("30SecondSignal")
-
+local sendSignal = ReplicatedStorage:FindFirstChild("30SecondSignal") or Instance.new("RemoteEvent", ReplicatedStorage)
+sendSignal.Name = "30SecondSignal"
 
 local countStarted = false
 
@@ -20,23 +20,23 @@ end
 while true do
 	local count = #Players:GetPlayers()
 
-	if count == 1 and not countStarted then
+	if count >= 2 and not countStarted then
 		countStarted = true
 
-		for seconds = 2, 0, -1 do
+		for seconds = 30, 0, -1 do
 			StartCountdown:FireAllClients(seconds)
 			task.wait(1)
 
 			if seconds == 0 then
-				StartCountdown:FireAllClients(0)
+				sendSignal:FireAllClients()
 				print("Game starting...")
 				TeleportPlayers(workspace.SpawnFarmMap.CFrame * CFrame.new(0, 2, 0))
 			end
 		end
 
-	--elseif count < 2 then
-	--	StartCountdown:FireAllClients("Waiting for players (" .. count .. ")")
-	--	countStarted = false
+	elseif count < 2 then
+		StartCountdown:FireAllClients("Waiting for players (" .. count .. ")")
+		countStarted = false
 	end
 
 	task.wait(1) -- update faster so player count changes are shown quickly
