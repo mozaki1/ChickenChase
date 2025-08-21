250b135c87e59beb9fd5aa324086bf330ee7ede4
agbfrank <cutieyousie3@gmail.com>
2025-08-21 01:26:52 +0100

    Revert "configured erros in rojo scaled guis"
    This reverts commit 98e37294ce1a1ab6c91f12781608e89cbeb47fa8.

--- src/client/ShopButton.client.lua
+++ src/client/ShopButton.client.lua
@@ -2,13 +2,14 @@ local button = script.Parent
 -- Try to find ShopFrame under MainFrame (the likely intended location)
 local mainFrame = button.Parent.Parent
 local shopFrame = nil
-local soundService = game:GetService("SoundService")
-
 if mainFrame then
 	shopFrame = mainFrame:FindFirstChild("ShopFrame")
 end
 
-
+-- Warn if ShopFrame is not found
+if not shopFrame then
+	warn("ShopFrame not found! Please make sure there is a Frame named 'ShopFrame' under MainFrame.")
+end
 
 -- Hide shop at start
 if shopFrame then
